6f9e0189e32f4ead55979db931cf15dd5c53d896
Aniketch45 <aniketchafekar2004@gmail.com>
2025-11-06 23:17:01 +0530

    practice inserting and deleting data using shell REPL

--- studapp/models.py
+++ studapp/models.py
@@ -12,3 +12,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name},{self.roll_no},{self.age},{self.email}"
+    
+   '''python manage.py shell
+ from studapp.models import Student
+>>> Student.objects.create(first_name="Aniket",last_name="Chafekar",roll_no='49',age='21',email="Abc1234@gmail.com",dob='2004-12-02',address="Vaijpur")
+    Student.objects.all()
+    Student.objects.filter(age='19')
+    Student.objects.all().order_by('roll_no')
+     Student.objects.all().order_by('-roll_no')
+      s1=Student.objects.get(roll_no='3')
+>>> s1.delete()'''
